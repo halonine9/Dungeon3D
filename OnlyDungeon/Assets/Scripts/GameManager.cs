caf9d3295c292acbd5ce33148a74d57629078d51
halonine9 <dlselhvxg@gmail.com>
2025-05-23 08:18:36 +0900

    [chore] TextMeshPro

---
+++ OnlyDungeon/Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header ("플레이어 정보")]
+    [SerializeField] private int curHealth;
+    [SerializeField] private int maxHealth;
+    
+}
